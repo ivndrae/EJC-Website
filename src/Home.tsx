e2954351ed7ff7c3dcb0d8e2d2541e5ae8141db9
whi-ming <whimingj@gmail.com>
2024-11-06 18:18:52 -0500

    updated footer

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,55 +4,61 @@ import { useState } from 'react';
 
 function Content() {
   return (
-    <div className="h-full w-full fixed inset-0 bg-[#A6242F] p-7 flex flex-col select-none overflow-y-auto">
-      <div className=" flex-col flex-1 container mx-auto p-5 justify-center">
-        <div className=" flex items-center justify-center mt-[20%]">
-          <img className=" flex w-[10rem] lg:w-[10rem] rotate-2 mr-10 mb-7" src={logo} />
-          <div className="flex flex-col">
-
-
-
-            <p className="text-white text-5xl lg:text-6xl  font-black text-center tracking-wider font-['Lato']">encode justice<br />canada |</p>
-            <p className=" text-white text-lg lg:text-xl text-center md:mt-5 font-['KoHo'] ">Fighting for Justice in the age of Artificial Intelligence.</p>
-          </div>          
+    <>
+    <div className= "bg-[#A6242F] relative -z-20">
+      <div className="min-h-[100vh] w-full inset-0 bg-[#A6242F] p-7 flex flex-col select-none mb-[50vh] rounded-[60px]">
+        <div className=" flex-col flex-1 container mx-auto p-5 justify-center">
+          <div className=" flex items-center justify-center mt-[20%]">
+            <img className=" flex w-[10rem] lg:w-[10rem] rotate-2 mr-10 mb-7" src={logo} />
+            <div className="flex flex-col">
+              <p className="text-white text-5xl lg:text-6xl  font-black text-center tracking-wider font-['Lato']">
+                encode justice<br/>canada |
+              </p>
+              <p className=" text-white text-lg lg:text-xl text-center md:mt-5 font-['KoHo'] ">
+                Fighting for Justice in the age of Artificial Intelligence.
+              </p>
+            </div>
+          </div>
+        <div className="relative w-full text-white max-w-screen-md mt-[50vh]">
+          <p className="text-white text-5xl md:text-5xl font-bold">
+            We are the Canadian Chapter of Encode Justice
+          </p>
+          <p className="text-2xl leading-10">
+            Encode Justice is a global, youth-led organization that fights for human rights accountability and justice under AI. Harnessing a global network of volunteers from all over the world, we champion informed AI policy and encourage youth to confront the challenges of the age of automation through
+            political advocacy, community organizing, educational programming, and content creation.
+          </p>
         </div>
-        <div className="relative w-full text-white max-w-screen-md mt-[50vh]" >
-            <p className="text-white text-5xl md:text-5xl font-bold">
-              We are the Canadian Chapter of Encode Justice
-            </p>
-            <p className="text-2xl leading-10">
-              Encode Justice is a global, youth-led organization that fights for human rights accountability and justice under AI. Harnessing a global network of volunteers from all over the world, we champion informed AI policy and encourage youth to confront the challenges of the age of automation through
-              political advocacy, community organizing, educational programming, and content creation.
-            </p>
+        <div className="flex justify-between">
+          <span></span>
+          <p className="text-right text-white w-[50vw] mt-[60vh] text-2xl leading-10">
+            <span className="text-4xl font-bold"> Our mission </span> is to promote AI literacy and ethics among Gen Z. We create educational resources, and host events such as our Speaker Series, Hackathons ,etc. for students and professionals to collaborate, with challenge, and inspire each other.
+          </p>
+        </div>
+        <div className=" mt-[30vh]">
+          <div className="text-white text-5xl md:text-5xl font-bold text-center">
+            Our Partners
           </div>
-          <div className="flex justify-between">
-            <span></span>
-            <p className="text-right text-white w-[50vw] mt-[60vh] text-2xl leading-10">
-              <span className="text-4xl font-bold"> Our mission </span> is to promote AI literacy and ethics among Gen Z. We create educational resources, and host events such as our Speaker Series, Hackathons ,etc. for students and professionals to collaborate, with challenge, and inspire each other.
-            </p>
-          </div>
-          
-          <div className=" mt-[30vh]">
-            <div className="text-white text-5xl md:text-5xl font-bold text-center">Our Partners</div>
-            <div>partners</div>
-          </div>
+          <div>partners</div>
+        </div>
       </div>
-      
-      <div className="fixed bottom-0 left-0 w-full text-white p-4 m-1 flex items-center">
-        <div className="flex flex-col w-full">
-          <div className="flex gap-1 md:gap-3 justify-center items-center m-3">
+    
+    <div>
+    <div className="border-blue-300 border-4 fixed bottom-0 right-0 text-white bg-[#8C1616] -z-10 w-full h-[60vh]">
+        <div className="border-yellow-300 border -2 flex flex-col w-full">
             <a className="flex w-14 h-14 items-center justify-center" href="https://www.instagram.com/encodecanada/"><i className="bi bi-instagram text-4xl"></i></a>
             <a className="flex w-14 h-14 items-center justify-center" href="https://twitter.com/EncodeCanada"><i className="bi bi-twitter-x text-4xl"></i></a>
             <a className="flex w-14 h-14 items-center justify-center" href="https://www.facebook.com/encodejustice/"><i className="bi bi-facebook text-4xl"></i></a>
             <a className="flex w-14 h-14 items-center justify-center" href="https://www.linkedin.com/company/encode-justice-canada/?trk=organization_guest_main-feed-card-text"><i className="bi bi-linkedin text-4xl"></i></a>
             <a className="flex w-14 h-14 items-center justify-center" href="mailto:encodejusticecanada@gmail.com"><i className="bi bi-envelope-fill text-4xl"></i></a>
-          </div>
           <div className="flex justify-center text-white font-['JetBrains_Mono']">
             Connect with us!
           </div>
         </div>
       </div>
-    </div>
+      </div>
+      </div>
+      </div>
+      </>
   )
 }
 
