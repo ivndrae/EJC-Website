eb7ec79bcee858914f860ae45a9d405b79278659
whi-ming <whimingj@gmail.com>
2024-11-07 13:08:08 -0500

    put on scroll functions, added a style.css

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,13 +5,14 @@ import { useState } from 'react';
 function Content() {
   return (
     <>
-    <div className= "bg-[#A6242F] relative -z-20">
-      <div className="min-h-[100vh] w-full inset-0 bg-[#A6242F] p-7 flex flex-col select-none mb-[50vh] rounded-[60px]">
+
+    <div className= "bg-[#A6242F] relative -z-20" >
+      <div className="min-h-[100vh] w-full inset-0 bg-[#A6242F] p-7 flex flex-col select-none mb-[50vh] rounded-[5rem]">
         <div className=" flex-col flex-1 container mx-auto p-5 justify-center">
           <div className=" flex items-center justify-center mt-[20%]">
             <img className=" flex w-[10rem] lg:w-[10rem] rotate-2 mr-10 mb-7" src={logo} />
             <div className="flex flex-col">
-              <p className="text-white text-5xl lg:text-6xl  font-black text-center tracking-wider font-['Lato']">
+              <p className="text-white text-5xl lg:text-6xl  font-black text-center tracking-wider font-['Lato'] ">
                 encode justice<br/>canada |
               </p>
               <p className=" text-white text-lg lg:text-xl text-center md:mt-5 font-['KoHo'] ">
@@ -43,15 +44,18 @@ function Content() {
       </div>
     
     <div>
-    <div className="border-blue-300 border-4 fixed bottom-0 right-0 text-white bg-[#8C1616] -z-10 w-full h-[60vh]">
-        <div className="border-yellow-300 border -2 flex flex-col w-full">
+    <div className="border-blue-300 border-4 fixed bottom-0 right-0 text-white bg-[#8C1616] -z-10 w-full h-[60vh] pt-36 px-20">
+      <div className="text-white text-[2.75rem] md:text-[2.75rem] font-bold">
+        Let's get to know each<br/> other <span className="italic">better</span>
+      </div>
+        <div className="border-yellow-300 border -2 flex w-full">
             <a className="flex w-14 h-14 items-center justify-center" href="https://www.instagram.com/encodecanada/"><i className="bi bi-instagram text-4xl"></i></a>
             <a className="flex w-14 h-14 items-center justify-center" href="https://twitter.com/EncodeCanada"><i className="bi bi-twitter-x text-4xl"></i></a>
             <a className="flex w-14 h-14 items-center justify-center" href="https://www.facebook.com/encodejustice/"><i className="bi bi-facebook text-4xl"></i></a>
             <a className="flex w-14 h-14 items-center justify-center" href="https://www.linkedin.com/company/encode-justice-canada/?trk=organization_guest_main-feed-card-text"><i className="bi bi-linkedin text-4xl"></i></a>
             <a className="flex w-14 h-14 items-center justify-center" href="mailto:encodejusticecanada@gmail.com"><i className="bi bi-envelope-fill text-4xl"></i></a>
           <div className="flex justify-center text-white font-['JetBrains_Mono']">
-            Connect with us!
+            Join our Slack!
           </div>
         </div>
       </div>
@@ -61,6 +65,20 @@ function Content() {
       </>
   )
 }
+let lastScrollY = 0
+const navbar = document.querySelector('nav')
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY
+  if(currentScrollY > lastScrollY){
+    console.log('scroll Down')
+
+    navbar?.classList.add('hidden')
+  } else{
+    console.log('scroll Up')
+    navbar?.classList.remove('hidden')
+  }
+  lastScrollY = currentScrollY
+})
 
 function Home() {
   const navigate = useNavigate();
@@ -69,7 +87,7 @@ function Home() {
     <div className="drawer drawer-end">
       <input id="my-drawer-4" type="checkbox" className="drawer-toggle" onChange={(e) => { setIsOpen(e.target.checked) }} />
       <div className="drawer-content">
-        <div className="lowercase fixed top-0 right-0 z-10 hidden lg:flex">
+        <nav className="lowercase fixed top-0 right-0 z-10 lg:flex">
           <div className="flex flex-col items-end m-2">
             <div
               className={` w-full flex-grow lg:flex lg:items-end lg:w-auto bg-transparent ${isOpen ? "block" : "hidden"}`}
@@ -96,7 +114,7 @@ function Home() {
               </div>
             </div>
           </div>
-        </div>
+        </nav>
         <Content />
         <label htmlFor="my-drawer-4" className="fixed flex right-0 top-0 z-10  mt-4 w-24 drawer-button btn bg-transparent border-none lg:hidden">
           <svg
