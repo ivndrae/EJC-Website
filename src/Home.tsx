7a45ace45a3881730c8094bcdf1d8c876a02ff75
whi-ming <whimingj@gmail.com>
2024-12-18 13:43:05 -0500

    trying redo the footer

--- src/Home.tsx
+++ src/Home.tsx
@@ -154,12 +154,12 @@ function Home() {
       </div>
       <div className='h-[600px] sm:h-[500px]'></div>
       </div>
-      <footer className=" bg-[#8C1616] fixed bottom-0 right-0 w-full h-full md:pt-36 md:px-20 z-10 text-[#F2BBBB]">
-        <div className='absolute bottom-0 mb-16 md:mb-12'>
-          <div className="tracking-wider leading-8 md:leading-[3rem] text-center md:text-left text-[2.25rem] md:text-[2.75rem] font-bold mb-10 md:mb-0">Let's get to know each<br className='hidden md:block'/> other <span className="italic">better</span></div>
-          <div className=" space-y-7 flex flex-col md:flex-row">        
-            <div className="md:w-[33.33%] mt-[5%] items-center">
-              <div className='flex space-x-[12%] text-[2rem] md:text-4xl items-center justify-center md:justify-start'>
+      <footer className=" bg-[#8C1616] fixed bottom-0 right-0 w-full h-full z-10 text-[#F2BBBB] ">
+        <div className='absolute bottom-0 mb-16 md:mb-12 border-4 border-yellow-200 w-full md:px-20'>
+          <div className="tracking-wider leading-8 md:leading-[3rem] text-center md:text-left text-[2.25rem] md:text-[3rem] font-bold mb-10 md:mb-0">Let's get to know each<br className='hidden md:block'/> other <span className="italic">better</span></div>
+          <div className=" space-y-7 flex flex-col md:flex-row border-4 border-green-200 justify-between w-full">        
+            <div className="mt-10 flex flex-col justify-between">
+              <div className='flex space-x-[12%] text-[2rem] md:text-4xl'>
                 <a href="https://www.instagram.com/encodecanada/"><i className="bi-instagram font-thin hover:text-[#f2f2f2] duration-300 "></i></a>
                 <a className=' fill-current hover:text-[#f2f2f2] duration-300' href="https://www.linkedin.com/company/encode-justice-canada/?trk=organization_guest_main-feed-card-text"><svg className='w-9 md:w-10 fill-current' xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 512 512"><path d="M116.5 500.2V170.7H7v329.5h109.5zM61.8 125.7c38.1 0 62-25.4 62-57-.8-32.3-23.9-57-61.3-57-37.5 0-62 24.7-62 57 0 31.6 23.8 57 60.5 57h.8zM177 500.2s1.5-298.6 0-329.5h109.6v47.7h-.8c14.4-22.4 40.4-55.5 99.5-55.5 72 0 126.1 47.1 126.1 148.3v189H402V324c0-44.3-15.9-74.5-55.5-74.5-30.3 0-48.3 20.4-56.2 40-3 7-3.6 17-3.6 26.8v184H177z" clipRule="evenodd"/></svg></a>
                 <a href="https://twitter.com/EncodeCanada"><i className=" bi-twitter-x hover:text-[#f2f2f2] duration-300"></i></a>
@@ -167,23 +167,30 @@ function Home() {
                 <a href="https://www.linkedin.com/company/encode-justice-canada/?trk=organization_guest_main-feed-card-text"><i className="bi-linkedin text-4xl"></i></a> */}
                 <a className=" w-9 md:w-10" href="https://www.facebook.com/encodejustice/"><svg className='w-10 fill-current hover:text-[#f2f2f2] duration-300' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 310" xmlSpace="preserve"><path d="M81.7 165.1h34V305a5 5 0 0 0 5 5h57.6a5 5 0 0 0 5-5V165.8h39a5 5 0 0 0 5-4.5l6-51.5a5 5 0 0 0-5-5.5h-45V72c0-9.8 5.2-14.7 15.6-14.7h29.4a5 5 0 0 0 5-5V5a5 5 0 0 0-5-5h-42.4c-7 0-31.5 1.4-50.8 19.2a53.3 53.3 0 0 0-17.7 47.3v37.8H81.7a5 5 0 0 0-5 5V160a5 5 0 0 0 5 5z"/></svg></a>
               </div>
-              <a className="text-[1.2rem] md:text-[1.35rem] hover:text-[#f2f2f2] flex text-center duration-300 mt-3" href="mailto:encodejusticecanada@gmail.com"><div className='m-auto md:m-0'>encodejusticecanada@gmail.com</div></a>  
+              <div className="text-[1.35rem] md:text-[1.75rem]">
+                Join our 
+                <a className="hover:text-[#f2f2f2] hover:underline duration-300" href="https://join.slack.com/t/encode-canada/shared_invite/zt-2qi3jy5si-trHbvtoDuc_r2ybjdmypAg" target='_blank'>
+                  <i class="bi bi-slack"></i> Slack! 
+                </a>
+              </div>
+              <a className="text-[1.2rem] md:text-[1.35rem] hover:text-[#f2f2f2] duration-300" href="mailto:encodejusticecanada@gmail.com"><div className=''>encodejusticecanada@gmail.com</div></a>  
             </div>
-            <div className="md:w-[33.33%] text-center md:m-auto text-[1.35rem] md:text-[1.75rem] md:pr-30 md:py-5">Join our <a className="hover:text-[#f2f2f2] hover:underline duration-300" href="https://join.slack.com/t/encode-canada/shared_invite/zt-2qi3jy5si-trHbvtoDuc_r2ybjdmypAg" target='_blank'><i class="bi bi-slack"></i> Slack! </a></div>
             
-            <form method="post" action="" name="contact-form" onSubmit={onFormSubmit} className=' md:w-[33.33%] text-[1.25rem] md:text-[1.5rem] leading-6 px-10 md:px-7 mr-10 w-full'>
+            
+            <form method="post" action="" name="contact-form" onSubmit={onFormSubmit} className=' md:w-[400px] text-[1.25rem] md:text-[1.5rem] leading-6 border'>
               Subscribe to our newsletter to stay connected with the latest updates from the Encode community.
-              <div className='flex space-x-4 pt-4 items-end mt-3'>
+              <div className='flex space-x-4 mt-7'>
                 <div className='rounded-[20px] border-[3px] border-[#f2bbbb] hover:border-white duration-300'>
                   <input name="Name" type="text" placeholder='name' className='bg-transparent placeholder-[#ffffff64] hover:placeholder-[#ffffffa1] pl-4 w-full outline-none focus:border-white hover:border-white text-white'/>
                   <hr className='w-[90%] m-auto border-inherit border-[1.5px]' />
                   <input name="Email" type="text" placeholder='email address' className='bg-transparent placeholder-[#ffffff64] hover:placeholder-[#ffffffa1] pl-4 w-full outline-none focus:border-white hover:border-white text-white'/>
                 </div>
-                <button type="submit" id="submit" className='border-[3px] border-[#f2bbbb] bg-[#f2bbbb] rounded-full w-[12.5%] h-9 text-center text-[#8C1616] hover:cursor-pointer hover:bg-[#f2f2f2] hover:border-[#f2f2f2] duration-300 '><i className="bi-arrow-right"></i></button>
+                <button type="submit" id="submit" className='border-[3px] border-[#f2bbbb] bg-[#f2bbbb] rounded-full w-[60px] h-9 text-center text-[#8C1616] hover:cursor-pointer hover:bg-[#f2f2f2] hover:border-[#f2f2f2] duration-300 '><i className="bi-arrow-right"></i></button>
               </div>
             </form>
           </div>
         </div>
+        <img className="min-w-[2400px] rotate-2 opacity-[0.07] fixed top-1 -left-40" src={logo} />
       </footer>
     </>
   )
